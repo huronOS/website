73c4ab1accee07a11527c4b7e7e0d640212b46c2
equetzal <equetzal@huronos.org>
2023-06-07 23:27:42 -0600

    Moving staging action to staging.huronos.org

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
 	url: "https://huronos.org",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/huronOS-website/",
+	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
