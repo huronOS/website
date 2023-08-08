e845a575249aa9de1a79325b334f1033869c0eee
equetzal <equetzal@huronos.org>
2023-08-07 23:54:09 -0600

    Make website only indexable in prod

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
 	organizationName: "huronOS", // Usually your GitHub org/user name.
 	projectName: "huronOS-website", // Usually your repo name.
 
+	noIndex: true,
 	onBrokenLinks: "warn",
 	onBrokenMarkdownLinks: "warn",
 
